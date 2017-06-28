cbb1c855307eeef9b0021fe5ad4d8ead25c67d73
Armand Fardeau <fardeauarmand@gmail.com>
2017-06-29 01:39:19 +0200

    add team member page

--- page-templates/project-team.php
+++ page-templates/project-team.php
@@ -1,7 +1,16 @@
 <?php
+/*
+ * Template Name: project team
+ * Description: Our project team
+ */
 /**
  * Created by PhpStorm.
  * User: armandfardeau
  * Date: 29/06/2017
  * Time: 01:15
  */
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+Timber::render( array( 'page-project-team.twig', 'page.twig' ), $context );
