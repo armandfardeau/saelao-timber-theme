7efa79378305832a9a97cb7d260516bbb9f918c6
Armand Fardeau <fardeauarmand@gmail.com>
2017-06-30 16:27:24 +0200

    add template and delete useless template

---
+++ page-templates/donate.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Template Name: donate team
+ * Description: donate model
+ */
+/**
+ * Created by PhpStorm.
+ * User: armandfardeau
+ * Date: 22/05/2017
+ * Time: 11:40
+ */
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+Timber::render( array( 'page-donate.twig'), $context );
